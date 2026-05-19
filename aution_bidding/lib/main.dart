f9530b1eef7b9210bbc3814cf6ee9a6ae28d748b
Gadhiya Vrund <gadhiyavrund246@gmail.com>
2026-05-19 11:23:59 +0530

    Refactor main.dart to set title to 'BidSync' and update theme colors; minor formatting adjustment in splash_screen.dart

--- aution_bidding/lib/main.dart
+++ aution_bidding/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'features/home/presentation/pages/offer_point_home_screen.dart';
+import 'screens/splash_screen.dart';
 
 void main() {
   runApp(const BidSyncApp());
@@ -13,126 +13,16 @@ class BidSyncApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'OfferPoint',
+      title: 'BidSync',
       theme: ThemeData(
         useMaterial3: true,
-        brightness: Brightness.dark,
+        brightness: Brightness.light,
         colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFF9AA7B8),
-          brightness: Brightness.dark,
-          surface: const Color(0xFF11161D),
-        ),
-        scaffoldBackgroundColor: const Color(0xFF090B10),
-        textTheme: const TextTheme(
-          displaySmall: TextStyle(
-            fontSize: 40,
-            fontWeight: FontWeight.w800,
-          ),
-          titleLarge: TextStyle(
-            fontSize: 22,
-            fontWeight: FontWeight.w700,
-          ),
-          titleMedium: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.w500,
-          ),
-          bodyMedium: TextStyle(
-            fontSize: 14,
-            fontWeight: FontWeight.w400,
-          ),
-      home: const SplashScreen(),
-    );
-  }
-}
-
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xffF4F6F8),
-
-      body: SafeArea(
-        child: Stack(
-          children: [
-
-            /// CENTER CONTENT
-            Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-
-                  /// LOGO
-                  Image.asset(
-                    'assets/images/logo.png',
-                    height: 180,
-                    fit: BoxFit.contain,
-                  ),
-
-                  const SizedBox(height: 20),
-
-                  /// TITLE
-                  ShaderMask(
-                    shaderCallback: (bounds) => const LinearGradient(
-                      colors: [
-                        Color(0xff0057B8),
-                        Color(0xff11B5AE),
-                      ],
-                    ).createShader(bounds),
-
-                    
-                  ),
-
-                  const SizedBox(height: 12),
-
-                  /// TAGLINE
-                
-                  const SizedBox(height: 40),
-
-                  /// LOADER
-                  const SizedBox(
-                    height: 32,
-                    width: 32,
-                    child: CircularProgressIndicator(
-                      strokeWidth: 3,
-                      color: Color(0xff0057B8),
-                    ),
-                  ),
-
-                  const SizedBox(height: 20),
-
-                  /// VERSION
-                  const Text(
-                    "Version 1.0.0",
-                    style: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 14,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-
-            /// COPYRIGHT BOTTOM
-            const Positioned(
-              bottom: 20,
-              left: 0,
-              right: 0,
-              child: Center(
-                child: Text(
-                  "© 2026 BidSync Technologies",
-                  style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 13,
-                  ),
-                ),
-              ),
-            ),
-          ],
+          seedColor: const Color(0xFF0057B8),
+          brightness: Brightness.light,
         ),
       ),
-      home: const OfferPointHomeScreen(),
+      home: const SplashScreen(),
     );
   }
 }
