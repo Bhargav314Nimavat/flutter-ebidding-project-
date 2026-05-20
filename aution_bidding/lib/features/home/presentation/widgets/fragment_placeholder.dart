21bfd671c5e7ce9c2d9511c27de40156d3e5b36d
Gadhiya Vrund <gadhiyavrund246@gmail.com>
2026-05-20 15:30:54 +0530

    making FPH statefull

--- aution_bidding/lib/features/home/presentation/widgets/fragment_placeholder.dart
+++ aution_bidding/lib/features/home/presentation/widgets/fragment_placeholder.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'package:flutter/material.dart';
 import '../pages/addbiddin.dart';
 import '../pages/offer_point_home_screen.dart';
@@ -28,33 +27,4 @@ class _FragmentPlaceholderState extends State<FragmentPlaceholder> {
     );
   }
 }
-=======
-import 'package:flutter/material.dart';
-import '../pages/addbiddin.dart';
-import '../pages/offer_point_home_screen.dart';
 
-class FragmentPlaceholder extends StatelessWidget {
-  const FragmentPlaceholder({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Navigator(
-      onGenerateRoute: (settings) {
-        switch (settings.name) {
-          case '/':
-            return MaterialPageRoute(
-              builder: (context) => const OfferPointHomeScreen(),
-            );
-
-          case '/addbidding':
-            return MaterialPageRoute(
-              builder: (context) => const AddBidding(),
-            );
-          default:
-            return null;
-        }
-      },
-    );
-  }
-}
->>>>>>> 1ee7ac50d07585c8195261b9408624f48003b329
